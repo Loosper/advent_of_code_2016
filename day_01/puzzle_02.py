ff3a528c7f26948e3e68915a3ce7a3d9aae4b9e1
Loosper <boian4o1@gmail.com>
2016-12-22 19:44:23 +0200

    Part 2 day 1

--- day_01/puzzle_02.py
+++ day_01/puzzle_02.py
@@ -4,7 +4,6 @@ directions = directions.split(', ')
 distance = [0] * 2
 horizontal = False
 direction = True
-
 places = set()
 
 for step in directions:
@@ -17,15 +16,13 @@ for step in directions:
             direction = not direction
 
     sign = int(step[1:])
-    if not direction:
-        sign *= -1
+    for point in range(sign):
+        if not direction:
+            point = -1
+        else:
+            point = 1
 
-    distance[0 if horizontal else 1] += sign
-
-    if tuple(distance) in places:
-        print(distance[0] + distance[1])
-    places.add(tuple(distance))
-
-
-print(distance[0] + distance[1])
-print(places)
+        distance[0 if horizontal else 1] += point
+        if tuple(distance) in places:
+            print(abs(distance[0]) + abs(distance[1]), end=', ')
+        places.add(tuple(distance))
